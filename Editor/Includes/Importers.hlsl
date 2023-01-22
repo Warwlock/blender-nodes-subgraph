d9f6713e20bbee36f631f9e70a9b31d20222c9a1
Warwlock <serhanbugrahan735@hotmail.com>
2023-01-22 14:11:44 +0300

    Update on Files

--- Editor/Includes/Importers.hlsl
+++ Editor/Includes/Importers.hlsl
@@ -1,33 +1,33 @@
 #ifndef __IncludeFiles_
  #define __IncludeFiles_ 
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/BrickTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/CheckerTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/GammaCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/GradientTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/HueSatValCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MagicTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MappingCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MapRangeCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MathCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MixRGBCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/MusgraveTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/NoiseTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/VectorMathCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/VoronoiTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/WaveTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/BrightContrastCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/SeparateCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/BlackbodyCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/CombineCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/ColorRampCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/RGBCurvesCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/RGBtoBWCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/ImageTextureCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/InvertCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/SimpleNoiseTexture.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/NormalMapCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/BumpCode.hlsl>
-	#include <Assets/Material Nodes Graph/Scripts/Editor/Includes/GeometryCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/BrickTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/CheckerTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/GammaCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/GradientTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/HueSatValCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MagicTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MappingCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MapRangeCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MathCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MixRGBCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/MusgraveTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/NoiseTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/VectorMathCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/VoronoiTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/WaveTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/BrightContrastCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/SeparateCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/BlackbodyCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/CombineCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/ColorRampCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/RGBCurvesCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/RGBtoBWCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/ImageTextureCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/InvertCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/SimpleNoiseTexture.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/NormalMapCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/BumpCode.hlsl>
+	#include <Packages/com.blendernodesgraph.core/Editor/Includes/GeometryCode.hlsl>
 
 TEXTURE2D(_Empty_Texture);
 #endif 
